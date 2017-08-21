4987d9504675f5abf55107b8bde9eac6ab9d3573
Carl Marquez <carl.louis.marquez@gmail.com>
2017-08-21 18:44:21 -0400

    Added wire reel servo
    - Added code for reeling the wire in and out
    - Removed debug messages

--- electromagnet_crane/electromagnet_crane.ino
+++ electromagnet_crane/electromagnet_crane.ino
@@ -13,11 +13,12 @@
  * - Arduino Uno
  * - USB Host Shield
  * - 2x MG995 180 degrees
+ * - MG995 360 degrees
  * - PS3 controller
  *
  * Created 2017-08-20
  * By Carl Marquez
- * Modified 2017-08-20
+ * Modified 2017-08-21
  * By Carl Marquez
  */
  
@@ -29,16 +30,12 @@ PS3USB ps3(&ps3Usb);
 
 Servo pan;
 Servo tilt;
+Servo wire;
 int8_t panAngle; // Horizontal turn
 int8_t tiltAngle; // Vertical tilt, limited to 70 degrees
 
 void setup() {
-  Serial.begin(115200);
-  if (ps3Usb.Init() == -1) {
-    Serial.println("USB Host Shield did not start");
-    while (true);
-  }
-  Serial.println("PS3 USB Library Started");
+  if (ps3Usb.Init() == -1) while (true);
 
   pan.attach(3);
   panAngle = 0;
@@ -47,6 +44,8 @@ void setup() {
   tilt.attach(4);
   tiltAngle = -45;
   tilt.writeMicroseconds(map(tiltAngle, -90, 90, 550, 2250));
+
+  wire.attach(5);
 }
 
 void loop() {
@@ -55,26 +54,30 @@ void loop() {
     // Moving the joystick left / right controls pan
     if (ps3.getAnalogHat(LeftHatX) < 50) {
       panAngle += (int8_t)(panAngle < 90);
-      Serial.println("LEFT");
     }
     if (ps3.getAnalogHat(LeftHatX) > 205) {
       panAngle -= (int8_t)(panAngle > -90);
-      Serial.println("RIGHT");
     }
 
     // Moving the joystick up / down controls tilt
     if (ps3.getAnalogHat(LeftHatY) < 50) {
       tiltAngle += (int8_t)(tiltAngle < -20);
-      Serial.println("UP");
     }
     if (ps3.getAnalogHat(LeftHatY) > 205) {
       tiltAngle -= (int8_t)(tiltAngle > -90);
-      Serial.println("DOWN");
     }
+
+    // Pressing up / down reels the wire out / in
+    if (ps3.getButtonPress(UP)) {
+      wire.writeMicroseconds(1000);
+    }
+    else if (ps3.getButtonPress(DOWN)) {
+      wire.writeMicroseconds(2000);
+    }
+    else wire.writeMicroseconds(1500);
     
     pan.writeMicroseconds(map(panAngle,-90,90,650,2300));
     tilt.writeMicroseconds(map(tiltAngle,-90,90,550,2200));
-    Serial.println(tiltAngle + 90);
   }
   _delay_ms(20);
 }
